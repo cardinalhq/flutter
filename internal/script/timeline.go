086e369d180043ec88a55696d057563e951cf065
Michael Graff <mgraff@cardinalhq.io>
2025-05-05 16:58:42 -0500

    refactor

--- internal/script/timeline.go
+++ internal/script/timeline.go
@@ -72,77 +72,105 @@ func (t *Timeline) MergeIntoConfig(cfg *config.Config) error {
 func mergeMetric(cfg *config.Config, metric Metric) error {
 	for _, variant := range metric.Variants {
 		id := makeID(metric, variant)
-		frequency := metric.Frequency
-		if frequency.Get() == 0 {
-			frequency = config.Duration{Duration: exporters.DefaultFrequency}
-		}
-		generators := []string{
-			id + "_noise",
-		}
-		for i := range len(variant.Timeline) {
-			generators = append(generators, id+"_ramp_"+strconv.Itoa(i))
-		}
-		// Add the metric to the config
-		action := config.ScriptAction{
-			Name: id,
-			Type: "metric",
-			Spec: specToMap(exporters.MetricGaugeSpec{
-				MetricExporterSpec: exporters.MetricExporterSpec{
-					Name:      metric.Name,
-					Type:      metric.Type,
-					Frequency: frequency.Get(),
-					Attributes: exporters.Attributes{
-						Resource:  metric.ResourceAttributes,
-						Datapoint: variant.Attributes,
-					},
-					Generators: generators,
-				},
-			}),
-		}
-		cfg.Script = append(cfg.Script, action)
+		frequency := getFrequency(metric.Frequency)
+		generators := generateGeneratorIDs(id, len(variant.Timeline))
 
-		// add a noise generator
-		action = config.ScriptAction{
-			Name: id + "_noise",
-			Type: "metricGenerator",
-			Spec: specToMap(generator.MetricGaussianNoiseSpec{
-				MetricGeneratorSpec: generator.MetricGeneratorSpec{
-					Type: "gaussianNoise",
-				},
-				Variation: 5,
-				Direction: "positive",
-			}),
+		if err := addMetricToConfig(cfg, id, metric, variant, frequency, generators); err != nil {
+			return err
 		}
-		cfg.Script = append(cfg.Script, action)
 
-		// Add the timeline to the config
-		prevStart := float64(0)
-		dpCount := len(variant.Timeline)
-		for i, dp := range variant.Timeline {
-			duration := dp.EndTs.Get() - dp.StartTs.Get()
-			if duration <= 0 {
-				duration = time.Second
-			}
-			action = config.ScriptAction{
-				Name: id + "_ramp_" + strconv.Itoa(i),
-				Type: "metricGenerator",
-				At:   dp.StartTs.Get(),
-				Spec: specToMap(generator.MetricRampSpec{
-					MetricGeneratorSpec: generator.MetricGeneratorSpec{
-						Type: "ramp",
-					},
-					Start:        prevStart,
-					Target:       dp.Median,
-					Duration:     duration,
-					PrestartZero: i != 0,
-					PostEndZero:  i > 0 && i != dpCount-1,
-				}),
-			}
-			prevStart = dp.Median
-			cfg.Script = append(cfg.Script, action)
+		if err := addNoiseGenerator(cfg, id); err != nil {
+			return err
+		}
+
+		if err := addTimelineToConfig(cfg, id, variant.Timeline); err != nil {
+			return err
 		}
 	}
+	return nil
+}
 
+func getFrequency(frequency config.Duration) time.Duration {
+	if frequency.Get() == 0 {
+		return exporters.DefaultFrequency
+	}
+	return frequency.Get()
+}
+
+func generateGeneratorIDs(id string, timelineLength int) []string {
+	generators := []string{id + "_noise"}
+	for i := 0; i < timelineLength; i++ {
+		generators = append(generators, id+"_ramp_"+strconv.Itoa(i))
+	}
+	return generators
+}
+
+func addMetricToConfig(cfg *config.Config, id string, metric Metric, variant Variant, frequency time.Duration, generators []string) error {
+	action := config.ScriptAction{
+		Name: id,
+		Type: "metric",
+		Spec: specToMap(exporters.MetricGaugeSpec{
+			MetricExporterSpec: exporters.MetricExporterSpec{
+				Name:      metric.Name,
+				Type:      metric.Type,
+				Frequency: frequency,
+				Attributes: exporters.Attributes{
+					Resource:  metric.ResourceAttributes,
+					Datapoint: variant.Attributes,
+				},
+				Generators: generators,
+			},
+		}),
+	}
+	cfg.Script = append(cfg.Script, action)
+	return nil
+}
+
+func addNoiseGenerator(cfg *config.Config, id string) error {
+	action := config.ScriptAction{
+		Name: id + "_noise",
+		Type: "metricGenerator",
+		Spec: specToMap(generator.MetricGaussianNoiseSpec{
+			MetricGeneratorSpec: generator.MetricGeneratorSpec{
+				Type: "gaussianNoise",
+			},
+			Variation: 5,
+			Direction: "positive",
+		}),
+	}
+	cfg.Script = append(cfg.Script, action)
+	return nil
+}
+
+func addTimelineToConfig(cfg *config.Config, id string, timeline []Segment) error {
+	prevStart := float64(0)
+	dpCount := len(timeline)
+	if dpCount > 0 {
+		prevStart = timeline[0].Median
+	}
+	for i, dp := range timeline {
+		duration := dp.EndTs.Get() - dp.StartTs.Get()
+		if duration <= 0 {
+			duration = time.Second
+		}
+		action := config.ScriptAction{
+			Name: id + "_ramp_" + strconv.Itoa(i),
+			Type: "metricGenerator",
+			At:   dp.StartTs.Get(),
+			Spec: specToMap(generator.MetricRampSpec{
+				MetricGeneratorSpec: generator.MetricGeneratorSpec{
+					Type: "ramp",
+				},
+				Start:        prevStart,
+				Target:       dp.Median,
+				Duration:     duration,
+				PrestartZero: i != 0,
+				PostEndZero:  i > 0 && i != dpCount-1,
+			}),
+		}
+		prevStart = dp.Median
+		cfg.Script = append(cfg.Script, action)
+	}
 	return nil
 }
 
